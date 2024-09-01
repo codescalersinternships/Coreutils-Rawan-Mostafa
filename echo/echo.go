84e79e7342e4681fbdd0ec8f1fccee68e4b358c2
RawanMostafa08 <rawanmostafa401@gmail.com>
2024-09-01 18:18:10 +0300

    feat: Adding echo utility

---
+++ echo/echo.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+
+	noNewLine := flag.Bool("n", false, "defines whether or not to add a new line")
+	flag.Parse()
+	for _, arg := range flag.Args() {
+		fmt.Printf("%s ", arg)
+	}
+	if !*noNewLine && len(flag.Args()) != 0 {
+		fmt.Print("\n")
+	}
+}
