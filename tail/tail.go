fc5eecb54403a5d332c4a7dd0ee1d584b27e0793
RawanMostafa08 <rawanmostafa401@gmail.com>
2024-09-01 14:44:19 +0300

    feat: Adding tail utility

---
+++ tail/tail.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+
+	var n int
+
+	flag.IntVar(&n, "n", 10, "defines the number fo lines to be printed")
+	flag.Parse()
+	data, err := os.ReadFile(flag.Args()[0])
+	check(err)
+
+	dataArray := strings.Split(string(data), "\n")
+	if n > len(dataArray) {
+		n = len(dataArray)
+	}
+	for i := len(dataArray) - n; i < len(dataArray); i++ {
+		fmt.Println(dataArray[i])
+	}
+}
